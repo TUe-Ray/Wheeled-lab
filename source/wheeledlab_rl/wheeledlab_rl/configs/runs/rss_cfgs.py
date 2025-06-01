c2af9f224f438ba58e04b0143f8d222e6e5555f8
Joost Leliveld <j.j.p.leliveld@student.tue.nl>
2025-06-01 12:43:22 +0200

    reward function change push for aws

--- source/wheeledlab_rl/wheeledlab_rl/configs/runs/rss_cfgs.py
+++ source/wheeledlab_rl/wheeledlab_rl/configs/runs/rss_cfgs.py
@@ -7,7 +7,7 @@ from wheeledlab_rl.configs import (
 @configclass
 class RSS_DRIFT_CONFIG(RslRlRunConfig):
     env_setup = EnvSetup(
-        num_envs=500,
+        num_envs=256,
         task_name="Isaac-MushrDriftRL-v0"
     )
     train = RLTrainConfig(
