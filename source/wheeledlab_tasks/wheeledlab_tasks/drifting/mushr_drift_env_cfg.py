cf121c3e7f2bdc1a15fed9761e7c52f20bd9004e
Joost Leliveld <j.j.p.leliveld@student.tue.nl>
2025-06-01 02:57:43 +0200

    reward function change push for aws

--- source/wheeledlab_tasks/wheeledlab_tasks/drifting/mushr_drift_env_cfg.py
+++ source/wheeledlab_tasks/wheeledlab_tasks/drifting/mushr_drift_env_cfg.py
@@ -741,7 +741,7 @@ class MushrDriftPlayEnvCfg(MushrDriftRLEnvCfg):
     """no terminations"""
 
     events: DriftEventsCfg = DriftEventsRandomCfg(
-        reset_root_state = EventTerm(
+        reset_robot = EventTerm(
             func=reset_root_state_along_track,
             params={
                 "dist_noise": 0.,
