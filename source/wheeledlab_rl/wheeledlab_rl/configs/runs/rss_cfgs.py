d5b5a0640d0336fae60e84335df8eeac7339c517
Joost Leliveld <j.j.p.leliveld@student.tue.nl>
2025-06-02 16:43:25 +0200

    new observations

--- source/wheeledlab_rl/wheeledlab_rl/configs/runs/rss_cfgs.py
+++ source/wheeledlab_rl/wheeledlab_rl/configs/runs/rss_cfgs.py
@@ -7,7 +7,7 @@ from wheeledlab_rl.configs import (
 @configclass
 class RSS_NAV_CONFIG(RslRlRunConfig):
     env_setup = EnvSetup(
-        num_envs=512,
+        num_envs=256,
         task_name="IsaacLab-OriginOneNavigation"
     )
     train = RLTrainConfig(
